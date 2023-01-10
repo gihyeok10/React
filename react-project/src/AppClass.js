69b982067e512549a856ac8c3dbcfae0d7dbce4f
kimgihyeok <gh9812301@naver.com>
2023-01-10 15:50:17 +0900

    클래스컴포넌트/라이프사이클

--- react-project/src/AppClass.js
+++ react-project/src/AppClass.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import BoxClass from "./component/BoxClass";
 export default class AppClass extends Component {
-  //클래스 생성할떄 쓰임
+  //클래스 생성할떄 쓰임 state만듦
    constructor(props){
     super(props)
     //state 쓰는 법
@@ -9,22 +9,33 @@ export default class AppClass extends Component {
         counter2:0,
         num:1,
         value:0
-    }
+    };
+    console.log("constructor")
 
    }
    
    increase = () => {
     this.setState({counter2:this.state.counter2 +1,
                     value:this.state.value +1})
+                    console.log("increase",this.state)
    };
 
-   
+   componentDidMount(){
+    //주로 API콜
+    console.log("componentDidMount");
+   }
+
+    componentDidUpdate(){
+        console.log("componentDidUpdate",this.state)
+    }
+   //UI 그리기
   render() {
+    console.log("render")
     return (
       <div>
         <div>counter:{this.state.counter2}</div>
         <button onClick={this.increase}>클릭!!</button>
-        <BoxClass num={this.state.value}></BoxClass>
+        {this.state.counter2 < 3 && <BoxClass num={this.state.value}></BoxClass>}
       </div>
     );
   }
--- react-project/src/component/BoxClass.js
+++ react-project/src/component/BoxClass.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 export default class BoxClass extends Component {
 
+    componentWillUnmount(){
+        console.log("bye~bye")
+    }
   render() {
     return (
       <div>Box{this.props.num}</div>
