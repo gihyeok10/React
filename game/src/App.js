0a80d1e7de8d13815bf2d97f171e30f8c09f6b33
kimgihyeok <gh9812301@naver.com>
2023-01-10 13:47:19 +0900

    onclick&object.keys

--- game/src/App.js
+++ game/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Box from "./component/Box"
@@ -6,17 +7,50 @@ import Box from "./component/Box"
 // 버튼을 클릭하면 내껀 내가 클릭한거. 컴터는 랜덤.
 // 결과를 가지고 누가 이겼는지 승패를 따짐.
 // 테두리 컬러 (이기면 -초록, 지면-빨강 , 비기면-검정)
+//가위 바위 보를 객체로 만듬(이름, 이미지)
+const choice = {
+
+  rock:{name:"Rock",
+        img:"https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-640x514.jpg"
+        },
+  scissors:{name:"Scissors",
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSPEAHC1qKLD8xMNYUEboms8MAbp1sbwqPXQ&usqp=CAU"},
+  paper:{name:"Paper",img:"https://cdn.shopify.com/s/files/1/0293/4669/3204/products/1_6dcc270e-871e-466b-bd56-2c655a323a56_4472x.jpg?v=1667954943"}
+}
+
+
+
 function App() {
+
+  const [userSelect,setUserSelect] = useState(null);
+  const [computerSelect,setComputerSelect] = useState(null);
+
+
+
+  const play = (userChoice)=> {
+    console.log("선택댐",userChoice)
+    setUserSelect(choice[userChoice])
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice)  
+  }
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수!
+    let randomItem = Math.floor(Math.random()* itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+
+  }
   return (
     <div>
     <div className='main-div'>
-      <Box user="YOU" result="WIN"></Box>
-      <Box user="Computer" result="LOSE"></Box>
+      <Box user="YOU" result="WIN" item={userSelect}></Box>
+      <Box user="Computer" result="LOSE" item={computerSelect}></Box>
     </div>
     <div className="main-div">
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>  
       </div>
     </div> 
   );
--- game/src/component/Box.js
+++ game/src/component/Box.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function Box(props) {
-
+  console.log(props);
   return (
     <div className="box">
         <h1>{props.user}</h1>
-        <img className="item-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSPEAHC1qKLD8xMNYUEboms8MAbp1sbwqPXQ&usqp=CAU"/>
-        <h2>{props.result}</h2>   
+        <img className="item-img" src={props.item && props.item.img}/>
+        <h2>{props.result}</h2> 
     </div>
   )
 }
