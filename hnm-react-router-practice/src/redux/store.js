72370097e17cf40ddfbd8346929df83c5067c71e
kimgihyeok <gh9812301@naver.com>
2023-01-17 14:51:17 +0900

    redux-툴(devtool)

--- hnm-react-router-practice/src/redux/store.js
+++ hnm-react-router-practice/src/redux/store.js
@@ -1,8 +1,9 @@
 import {createStore,applyMiddleware} from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 // import productReducer from "./reducers/productReducer";
 import rootReducer from "./reducers/index" //index로 한 이유가 자동으로 읽어오기 때문
-let store = createStore(rootReducer,applyMiddleware(thunk));
+let store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)));
 //미들웨어 사용 준비
 
 export default store;
